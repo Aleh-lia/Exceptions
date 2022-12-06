e176e4f589ef4b8fb4ec1aee1c13458f49baf704
Aleh-lia <liavitski.oleh@yandex.by>
2022-12-06 22:51:03 +0300

    Lesson 1

---
+++ src/main/java/HomeWork/Task_4.java
@@ -0,0 +1,43 @@
+package HomeWork;
+
+import java.util.Random;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        int[] oneArr = getFilledArr(10);
+        int[] twoArr = getFilledArr(8);
+        System.out.println(divArr(oneArr, twoArr));
+
+
+        int[] divArr = divArr(oneArr, twoArr);
+        printArr(divArr);
+
+        int[] badArr = divArr(null, twoArr);
+    }
+
+    public static int[] divArr(int[] oneArr, int[] twoArr) {
+
+        if (oneArr == null || twoArr == null) throw new RuntimeException("Нет массивов!!!");
+        if(oneArr.length != twoArr.length) throw new RuntimeException("Длины массивов не равны!!!");
+        int[] resultArr = new int[oneArr.length];
+        for (int i = 0; i < resultArr.length; i++) {
+            resultArr[i] = oneArr[i]/twoArr[i];
+        }
+        return resultArr;
+    }
+
+    public static int[] getFilledArr(int length){
+        int[] array = new int[length];
+        Random rnd = new Random();
+        for (int i = 0; i < length; i++) {
+            array[i] = rnd.nextInt(1,20);
+        }
+        return array;
+    }
+
+    public static void printArr(int[] arr){
+        for (int n: arr){
+            System.out.println(n);
+        }
+    }
+}
